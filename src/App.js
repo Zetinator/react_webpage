c9f8dc3716bbe7c859ec078fe88b3031f41789fc
Zetinator <erick_zetina@hotmail.com>
2020-02-22 17:14:47 -0600

    nav bar looks ready

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<NavBar />
-				<main>
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</main>
 			</header>
+			<main class="App-main">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</main>
 		</div>
 	);
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -8,45 +7,21 @@ import ListItemText from "@material-ui/core/ListItemText";
 
 import "./NavBar.css";
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-	drawer: {
-		width: drawerWidth,
-		flexShrink: 0
-	},
-	drawerPaper: {
-		width: drawerWidth,
-		color: "blue"
-	},
-}));
-
 export default function NavBar() {
-	const classes = useStyles();
-
 	return (
 		<div className="nav-bar">
-			<Drawer
-				className={classes.drawer}
-				variant="permanent"
-				classes={{
-					paper: classes.drawerPaper
-				}}
-				anchor="left"
-			>
-				<img
-					class="profile-bild"
-					src="https://zetinator.github.io/img/profile.webp"
-					alt="zetinator's profile picture"
-				/>
-				<List>
-					{["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-			</Drawer>
+			<img
+				className="profile-bild"
+				src="https://zetinator.github.io/img/profile.webp"
+				alt="zetinator's profile picture"
+			/>
+			<List>
+				{["Experience", "Education", "Skills", "Interests", "Awards"].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
 		</div>
 	);
 }
