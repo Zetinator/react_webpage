00568bb0790e079e682ac27427f9f83e76609b4f
Zetinator <erick_zetina@hotmail.com>
2020-02-24 17:36:06 -0600

    languages now column wise

--- src/Languages.js
+++ src/Languages.js
@@ -4,7 +4,7 @@ export default function Languages() {
 	return (
 		<div className="languages">
 			<h1>Languages</h1>
-			<div className="job-row">
+			<div>
 				<h3>- SPANISH (MOTHER TONGUE)</h3>
 				<h3>- ENGLISH (FLUENT 567 POINTS TOEFL)</h3>
 				<h3>- GERMAN (FLUENT B2)</h3>
